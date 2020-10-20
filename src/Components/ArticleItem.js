3b0c2dc2501ca61198b71dcadda1dfd2719c6313
James <jh97uk@icloud.com>
2020-10-20 18:34:29 +0100

    Added responsive compatibility for mobile

--- src/Components/ArticleItem.js
+++ src/Components/ArticleItem.js
@@ -14,8 +14,8 @@ class ArticleItem extends Component{
         return(
         <Card style={{marginBottom:15}}>
             <CardContent>
-                <Typography variant='h6' style={{width:"100%"}} style={{cursor:'pointer'}} onClick={()=>this.props.onSelected(this.props.article)}>{this.props.article.title}</Typography>
-        <Typography color="textSecondary" gutterBottom style={{fontSize:12}}>{this.props.article.score} points | By {this.props.article.by} | {moment(new Date(this.props.article.time*1000)).fromNow()} | {this.props.article.descendants} comments</Typography>
+                <Typography variant='h6' style={{width:"100%"}} style={{cursor:'pointer'}} onClick={()=>this.props.onSelected(this.props.article.id)}>{this.props.article.title}</Typography>
+                <Typography color="textSecondary" gutterBottom style={{fontSize:12}}>{this.props.article.score} points | By {this.props.article.by} | {moment(new Date(this.props.article.time*1000)).fromNow()} | {this.props.article.descendants} comments</Typography>
             </CardContent>
         </Card>)
     }
--- src/Main.js
+++ src/Main.js
@@ -5,8 +5,11 @@ import AppBar from '@material-ui/core/AppBar';
 import {Toolbar} from '@material-ui/core';
 
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import Hidden from '@material-ui/core/Hidden';
 
+import Button from '@material-ui/core/Button';
+
+import Paper from '@material-ui/core/Paper';
 import ContentMain from './Views/ContentMain';
 import ContentSidebar from './Views/ContentSidebar.js';
 
@@ -15,34 +18,43 @@ import axios from 'axios';
 class Main extends Component{
     constructor(props){
         super(props);
+        console.log(props);
         this.state = {
             articles:[],
             selectedArticle:undefined,
+            hideArticleView:(this.props.location.pathname == "/")
         }
         this.onArticleSelected = this.onArticleSelected.bind(this);
     }
 
     onArticleSelected(articleId){
         this.props.history.push('/'+articleId);
+        this.setState({hideArticleView:false})
     }
 
     render(){
+        console.log(this.state.hideArticleView)
         return(
             <div style={{display: "flex",
             flexFlow: "column",
             height: "100vh"}}>
                 <AppBar position="static">
                     <Toolbar>
+                        <Hidden mdUp smDown={this.state.hideArticleView}><Button onClick={()=>this.setState({hideArticleView:true})}>Back</Button></Hidden>
                     </Toolbar>
                 </AppBar>
                 <Grid container style={{flex: "1 1 auto"}}>
-                    <Paper component={Grid} item sm={4} md={3}>
-                        <ContentSidebar onArticleSelected={this.onArticleSelected}/>
-                    </Paper>
-                    <Grid item sm={8} md={9}>
+                    <Hidden smDown={!this.state.hideArticleView}>
+                        <Paper component={Grid} item sm={12} md={3}>
+                            <ContentSidebar onArticleSelected={this.onArticleSelected}/>
+                        </Paper>
+                    </Hidden>
+                    <Hidden smDown={this.state.hideArticleView}>
+                    <Grid item sm={12} md={9}>
                         <Route exact path={this.props.match.path}  component={(props)=><ContentMain {...props}/>} />
                         <Route exact path="/:articleId"  component={(props)=><ContentMain {...props}/>} />
                     </Grid>
+                    </Hidden>
                 </Grid>
             </div>
         );
