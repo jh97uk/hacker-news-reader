6d244e97aa1e2fd6e8ed221d1d45fb0418837cc1
James <jh97uk@icloud.com>
2020-10-25 17:00:34 +0000

    Fixed select article message not showing

--- src/Components/ArticleSummary.js
+++ src/Components/ArticleSummary.js
@@ -7,7 +7,7 @@ import Paper from '@material-ui/core/Paper';
 function ArticleSummary(props){
     return(
         <Paper style={{padding:20, backgroundColor:'#f4f4f4'}} square>
-            {props.article ? 
+            {Object.keys(props.article).length > 1 ? 
                 <div>
                     <a href={props.article.url} style={{color:'black', textDecoration:'none'}}><Typography variant="h5">{props.article.title}</Typography></a>
                     <Typography color="textSecondary" gutterBottom style={{fontSize:12}}>{props.article.score} points | By {props.article.by} | {moment(new Date(props.article.time*1000)).fromNow()} | {props.article.descendants} comments</Typography>
